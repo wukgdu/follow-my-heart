e025a077d326a487807f9298fd559bc1639b035d
wukgdu <wukgdu365@yahoo.com>
2018-02-19 23:11:30 +0800

    defun

--- zip.lisp
+++ zip.lisp
@@ -1,14 +1,33 @@
-(set-macro-character #\Z #'zip-reader)
+#|
+(defun zip-helper (fn lists)
+  (labels ((zip-iter (lst result)
+             (let ((x (mapcar #'car lst)))
+               (cond ((some #'null lst) (nreverse result))
+                     (t (zip-iter (mapcar #'cdr lst) (push (apply fn x) result)))))))
+    (zip-iter lists nil)))
 
 (defun zip-reader (stream char)
   (declare (ignore char))
   (let ((fn 'list))
     (if (not (char= (peek-char nil stream nil nil t) #\space))
         (setf fn (read stream t nil t)))
-    `(lambda (xlist ylist)
-       (mapcar #'(lambda (x y) (funcall ',fn x y))
-               xlist
-               ylist))))
+    ;`(lambda (&rest lists)
+    ;   `(mapcar #'(lambda (&rest args) (apply ',',fn args))
+    ;            ,@(mapcar #'(lambda (x) `',x) lists)))))
+    `(lambda (&rest lists)
+       (zip-helper ',fn lists))))
 
-;; (Z '(1 2 3) '(4 5 6)) => ((1 4) (2 5) (3 6))
-;; (Z+ '(1 2 3) '(4 5 6)) => (5 7 9)
+(set-macro-character #\Z #'zip-reader)
+;; (print (Z '(1 2 3) '(1 2 3) '(1 2 3)))
+;; (print (Z+ '(1 2 3) '(1 2 3) '(1 2 3)))
+|#
+
+(defun zip (fn &rest lists)
+  (labels ((zip-iter (lst result)
+             (let ((x (mapcar #'car lst)))
+               (cond ((some #'null lst) (nreverse result))
+                     (t (zip-iter (mapcar #'cdr lst) (push (apply fn x) result)))))))
+    (zip-iter lists nil)))
+
+;; (zip 'list '(1 2 3) '(1 2 3) '(1 2 3))
+;; (zip '+ '(1 2 3) '(1 2 3) '(1 2 3))
