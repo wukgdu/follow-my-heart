24992d07c001d86b73c7840e5a006b16ef1a857f
wukgdu <wukgdu365@yahoo.com>
2018-04-09 22:45:28 +0800

    delete a line

--- scan-int.lisp
+++ scan-int.lisp
@@ -13,7 +13,6 @@
          (lst2 dims (cdr lst2))
          (start (car dims) (+ (* start (car lst1)) (cadr lst2))))
       ((null (cdr lst2))
-       ;(setf start (* start (car lst1)))
        (setf start (reduce #'* lst1 :initial-value start))
        (make-array lst1
                    :displaced-to arr
