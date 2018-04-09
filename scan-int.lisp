ad80c2d92618e3534331bff1d9511ff7435d3298
wukgdu <wukgdu365@yahoo.com>
2018-04-09 22:43:32 +0800

    not only last dimension

--- scan-int.lisp
+++ scan-int.lisp
@@ -12,13 +12,17 @@
     (do ((lst1 dimens (cdr lst1))
          (lst2 dims (cdr lst2))
          (start (car dims) (+ (* start (car lst1)) (cadr lst2))))
-      ((null (cdr lst1))
-       (setf start (* start (car lst1)))
-       (make-array (car lst1)
+      ((null (cdr lst2))
+       ;(setf start (* start (car lst1)))
+       (setf start (reduce #'* lst1 :initial-value start))
+       (make-array lst1
                    :displaced-to arr
                    :displaced-index-offset start)))))
 
+#|
 (defvar arr (make-array '(2 3 4)))
 (defvar strnum "1 2 3 4")
 (scan-int strnum 4 (get-dimension arr 1 1))
 (print arr)
+(print (get-dimension arr 1))
+|#
